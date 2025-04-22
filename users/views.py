4f82077a7af5571c81fd227cf6ee010c8b7554cd
Sangmin-Bae <simon.bae825@gmail.com>
2025-04-22 19:33:47 +0900

    refactor: allow unauthenticated access to SignUpView [T1-B2]
    - 회원가입 API(SignUpView)에 대해 인증 없이 접근 가능하도록 명시
    - permission_classes에 AllowAny 설정
    - authentication_classes를 빈 리스트로 선언하여 기본 인증 방식 제거
    - 보안 정책 명확화 및 향후 설정 변경에 따른 API 동작 안정성 확보

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -9,6 +10,9 @@ class SignUpView(APIView):
     """
     사용자 회원가입 요청을 처리하는 API 뷰
     """
+
+    authentication_classes = []
+    permission_classes = [AllowAny]
 
     def post(self, request):
         """
